a3dd5eb1df1f46c7ec9500856a3b8be35dc836b6
razeeman <freeraz@mail.ru>
2025-06-01 15:27:19 +0300

    fix multiselect for currently counting untracked time

--- domain/src/main/java/com/example/util/simpletimetracker/domain/record/model/MultiSelectedRecordId.kt
+++ domain/src/main/java/com/example/util/simpletimetracker/domain/record/model/MultiSelectedRecordId.kt
@@ -1,12 +1,18 @@
 package com.example.util.simpletimetracker.domain.record.model
 
 sealed interface MultiSelectedRecordId {
-    data class Tracked(val id: Long) : MultiSelectedRecordId
+    val id: Long
+
+    data class Tracked(override val id: Long) : MultiSelectedRecordId
 
     data class Untracked(
         val timeStartedTimestamp: Long,
         val timeEndedTimestamp: Long,
-    ) : MultiSelectedRecordId
+    ) : MultiSelectedRecordId {
+        override val id: Long = timeStartedTimestamp
+        override fun equals(other: Any?): Boolean = (other as? Untracked)?.id == id
+        override fun hashCode(): Int = id.hashCode()
+    }
 
-    data class Running(val id: Long) : MultiSelectedRecordId
+    data class Running(override val id: Long) : MultiSelectedRecordId
 }
