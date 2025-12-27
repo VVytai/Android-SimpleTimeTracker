f36aa8fe3aead2fa1d6c6d5c07e0995768c813ff
razeeman <freeraz@mail.ru>
2025-12-27 10:22:50 +0300

    fix room scheme export

--- data_local/build.gradle.kts
+++ data_local/build.gradle.kts
@@ -5,20 +5,17 @@ plugins {
     alias(libs.plugins.gradleLibrary)
     alias(libs.plugins.kotlin)
     alias(libs.plugins.ksp)
+    alias(libs.plugins.room)
 }
 
 applyAndroidLibrary()
 
 android {
-    defaultConfig {
-        javaCompileOptions {
-            annotationProcessorOptions {
-                arguments += mapOf("room.schemaLocation" to "$projectDir/schemas")
-            }
-        }
-    }
-
     namespace = "${Base.namespace}.data_local"
+}
+
+room {
+    schemaDirectory("$projectDir/schemas")
 }
 
 dependencies {
