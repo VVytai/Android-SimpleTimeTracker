b16df2e91c190cbb66b437824eb134c669dcc5da
razeeman <freeraz@mail.ru>
2026-02-08 10:28:01 +0300

    add lazy suspend

---
+++ domain/src/main/java/com/example/util/simpletimetracker/domain/base/LazySuspend.kt
@@ -0,0 +1,16 @@
+package com.example.util.simpletimetracker.domain.base
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.async
+
+fun <T> CoroutineScope.suspendLazy(
+    initializer: suspend CoroutineScope.() -> T
+) = object : SuspendLazy<T> {
+    private val deferred = async(start = CoroutineStart.LAZY, block = initializer)
+    override suspend operator fun invoke(): T = deferred.await()
+}
+
+interface SuspendLazy<T> {
+    suspend operator fun invoke(): T
+}
