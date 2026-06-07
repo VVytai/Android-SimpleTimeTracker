c327b7087170f2bbc9f687e9ba858d49c0caf33e
razeeman <freeraz@mail.ru>
2026-06-07 22:00:00 +0300

    remove feature dependencies from main module

--- features/feature_running_records/src/main/java/com/example/util/simpletimetracker/feature_running_records/di/RunningRecordsModule.kt
+++ features/feature_running_records/src/main/java/com/example/util/simpletimetracker/feature_running_records/di/RunningRecordsModule.kt
@@ -1,13 +1,19 @@
 package com.example.util.simpletimetracker.feature_running_records.di
 
+import com.example.util.simpletimetracker.core.model.NavigationTab
+import com.example.util.simpletimetracker.core.model.NavigationTabKey
+import com.example.util.simpletimetracker.core.model.NavigationTabProvider
 import com.example.util.simpletimetracker.feature_running_records.api.OnShortcutClickInteractor
 import com.example.util.simpletimetracker.feature_running_records.api.RecordsShortcutsViewDataInteractor
 import com.example.util.simpletimetracker.feature_running_records.interactor.OnShortcutClickInteractorImpl
 import com.example.util.simpletimetracker.feature_running_records.interactor.RecordsShortcutsViewDataInteractorImpl
+import com.example.util.simpletimetracker.feature_running_records.view.RunningRecordsFragment
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import dagger.multibindings.IntoMap
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -18,4 +24,13 @@ interface RunningRecordsModule {
 
     @Binds
     fun bindOnShortcutClickInteractor(impl: OnShortcutClickInteractorImpl): OnShortcutClickInteractor
+
+    companion object {
+        @Provides
+        @IntoMap
+        @NavigationTabKey(NavigationTab.RunningRecords::class)
+        fun bindNavigationTab(): NavigationTabProvider {
+            return NavigationTabProvider { RunningRecordsFragment.newInstance() }
+        }
+    }
 }
--- features/feature_settings/src/main/java/com/example/util/simpletimetracker/feature_settings/di/SettingsModule.kt
+++ features/feature_settings/src/main/java/com/example/util/simpletimetracker/feature_settings/di/SettingsModule.kt
@@ -1,15 +1,21 @@
 package com.example.util.simpletimetracker.feature_settings.di
 
+import com.example.util.simpletimetracker.core.model.NavigationTab
+import com.example.util.simpletimetracker.core.model.NavigationTabKey
+import com.example.util.simpletimetracker.core.model.NavigationTabProvider
 import com.example.util.simpletimetracker.feature_settings.api.OnSettingChangedInteractor
 import com.example.util.simpletimetracker.feature_settings.api.SettingsCardOrderMapper
 import com.example.util.simpletimetracker.feature_settings.api.SettingsOrderChangeInteractor
 import com.example.util.simpletimetracker.feature_settings.interactor.OnSettingChangedInteractorImpl
 import com.example.util.simpletimetracker.feature_settings.interactor.SettingsOrderChangeInteractorImpl
 import com.example.util.simpletimetracker.feature_settings.mapper.SettingsMapper
+import com.example.util.simpletimetracker.feature_settings.view.SettingsFragment
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import dagger.multibindings.IntoMap
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -23,4 +29,13 @@ interface SettingsModule {
 
     @Binds
     fun bindSettingsOrderChangeInteractor(impl: SettingsOrderChangeInteractorImpl): SettingsOrderChangeInteractor
+
+    companion object {
+        @Provides
+        @IntoMap
+        @NavigationTabKey(NavigationTab.Settings::class)
+        fun bindNavigationTab(): NavigationTabProvider {
+            return NavigationTabProvider { SettingsFragment.newInstance() }
+        }
+    }
 }
